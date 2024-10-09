e4ec9ae191a5731e9a7d811f2636f62280fd9b82
Javier Vargas - Pc Legion <javieravargasestudiante@gmail.com>
2024-10-08 20:49:06 -0500

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Countries from './components/Countries'
+import './scss/App.scss'
+
+function App() {
+
+    return (
+        <>
+            <div className='main'>
+                <h1>Banderas de los Países.</h1>
+                <Countries />
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Countries.tsx
@@ -0,0 +1,12 @@
+import ListCountries from "./ListCountries"
+
+const Countries = () => {
+    return (
+        <>
+            <h2>Países....</h2>
+            <ListCountries />
+        </>
+    )
+}
+
+export default Countries
