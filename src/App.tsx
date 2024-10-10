f62c8b96791f95766bc3e20c6c567b63c2a99574
Javier Vargas - Pc Legion <javieravargasestudiante@gmail.com>
2024-10-10 18:01:09 -0500

    Arreglo pequeños detalles

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ function App() {
 
     return (
         <>
-            <div className='main'>
+            <header>
                 <h1>Banderas de los Países.</h1>
+            </header>
+            <div className='main'>
                 <Countries />
             </div>
+            <footer>
+                <p>Elaborado por Javier A. Vargas V.</p>
+                <p>2024</p>
+            </footer>
         </>
     )
 }
--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -3,7 +3,6 @@ import ListCountries from "./ListCountries"
 const Countries = () => {
     return (
         <>
-            <h2>Países....</h2>
             <ListCountries />
         </>
     )
